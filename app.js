2d00fbdb389c4f0c624fa772f81d2bb32ff95d46
logicaloffice <logicalofficeatx@gmail.com>
2022-10-24 22:15:44 -0500

    Changing port to 443

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
     res.send();
 });
 
-app.listen(3000, function () {
-    console.log("server is up at port 3000");
+app.listen(443, function () {
+    console.log("server is up at port 443");
 });
